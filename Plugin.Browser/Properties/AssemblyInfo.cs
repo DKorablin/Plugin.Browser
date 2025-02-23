6ebae70a915dea8aa48085342ec303acc2b3cf4f
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 00:22:50 +0300

    Unified plugins deployment

--- Plugin.Browser/Properties/AssemblyInfo.cs
+++ Plugin.Browser/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("7476853a-3a40-4d5f-a5b5-a00f1dc4d24c")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=86")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Browser")]
-[assembly: AssemblyDescription("Using ebedded Microsoft © Internet Explorer © brower in the Windows Forms application")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Web Browser Windows Plugin")]
+[assembly: AssemblyDescription("Using ebedded Microsoft © Internet Explorer © browser for Windows Forms application")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2009-2016")]
 #endif
